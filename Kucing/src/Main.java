546103296a5b597cdda0f259c350dca56f2d571c
Hiroyuuki01 <denso572@gmail.com>
2024-03-04 12:18:46 +0700

    first commit

---
+++ Kucing/src/Main.java
@@ -0,0 +1,21 @@
+class Main {
+    public String nama = "Uhuy";
+    public char kelamin = 'L';
+    public int umur = 1;
+    public void namaKucing() {
+        System.out.println("Nama: " +nama);
+    }
+    public void kelaminKucnig() {
+        System.out.println("Id " +kelamin);
+    }
+    public void umurKucing() {
+        System.out.println("Umur: " +umur);
+    }
+
+    public static void main(String[] args) {
+        Main kelinci = new Main();
+        kelinci.namaKucing();
+        kelinci.kelaminKucnig();
+        kelinci.umurKucing();
+    }
+}
